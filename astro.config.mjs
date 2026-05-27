aecd010a82875de90dbb026f2fc121cf2a56c866
Krish Sridhar <skrish18@umd.edu>
2026-05-26 22:01:58 -0400

    fix tailwind config for astro 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://krishtheduck.github.io',
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
